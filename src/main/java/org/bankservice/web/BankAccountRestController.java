c58aaccf79e21c5d6779e91e8f2aa2a2ad6526c3
Reda-23 <readhajjaj@gmail.com>
2022-11-03 20:13:08 +0100

    created the update & delete functions

--- src/main/java/org/bankservice/web/BankAccountRestController.java
+++ src/main/java/org/bankservice/web/BankAccountRestController.java
@@ -3,11 +3,14 @@ package org.bankservice.web;
 
 import org.bankservice.entities.BankAccount;
 import org.bankservice.repository.BankAccountRepository;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.web.bind.annotation.*;
 
 
-
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -36,5 +39,23 @@ public class BankAccountRestController {
         return bankAccountRepository.save(bankAccount);
     }
 
+    @PutMapping("/accounts/{id}")
+    BankAccount update(@RequestBody BankAccount bankAccount ,@PathVariable String id){
+        BankAccount bankAccount1 = bankAccountRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("The Id Doesn't Match The Resources"));
+
+        bankAccount1.setBalance(bankAccount.getBalance());
+        bankAccount1.setCreatedAt(new Date());
+        bankAccount1.setType(bankAccount.getType());
+        bankAccount1.setCurrency(bankAccount.getCurrency());
+
+        return bankAccountRepository.save(bankAccount1);
+    }
+
+    @DeleteMapping("/accounts/{id}")
+    public void delete(@PathVariable String id){
+        BankAccount bankAccount = bankAccountRepository.findById(id).orElseThrow();
+        bankAccountRepository.delete(bankAccount);
+    }
 
 }
